28c4e8f65d391210664604fdbf18451503042f1d
Мурат Нысамбаев <nysambaev.m@cp.unipark.kz>
2021-01-31 17:54:52 +0600

    Added unit tests coverage report

--- api/tests/Functional/HomeTest.php
+++ api/tests/Functional/HomeTest.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Test\Functional;
 
-
+/**
+ * @coversNothing
+ */
 class HomeTest extends WebTestCase
 {
+
     public function testMethod(): void
     {
         $response = $this->app()->handle(self::json('POST', '/'));
