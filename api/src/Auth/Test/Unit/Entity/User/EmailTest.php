56ee7cb1527d61743aad33df95cb66508a108bb0
Мурат Нысамбаев <nysambaev.m@cp.unipark.kz>
2021-02-02 16:34:31 +0600

    Added join request use case

---
+++ api/src/Auth/Test/Unit/Entity/User/EmailTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth\Test\Unit\Entity\User;
+
+
+use App\Auth\Entity\User\Email;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+class EmailTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $email = new Email($value = 'email@app.test');
+        $this->assertEquals($value, $email->getValue());
+    }
+
+    public function testCase(): void
+    {
+        $email = new Email('EmAil@app.test');
+        $this->assertEquals('email@app.test', $email->getValue());
+    }
+
+    public function testIncorrect(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Email('not-email');
+    }
+
+    public function testEmpty(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Email('');
+    }
+}
