cfa37b1079458200f63e0972d34f5e2268f7eba6
Murat <nysambaev.m>
2022-06-19 18:57:35 +0600

    Moved error middleware into container

--- api/config/middleware.php
+++ api/config/middleware.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 
 use Psr\Container\ContainerInterface;
 use Slim\App;
+use Slim\Middleware\ErrorMiddleware;
 
 return static function (App $app, ContainerInterface $container): void {
-    /** @psalm-var array{debug:bool,env:string} */
-    $config = $container->get('config');
-    $app->addErrorMiddleware($config['debug'], $config['env'] !== 'test', true);
+    $app->add(ErrorMiddleware::class);
 };
