dce96821752180dbf959cf09bd693c0bca0b6283
Murat <nysambaev.m>
2022-06-26 19:16:25 +0600

    Added validation translations

--- api/config/common/translator.php
+++ api/config/common/translator.php
@@ -13,13 +13,28 @@ return [
          * @psalm-var array{lang:string,resources:array<string[]>} $config
          */
         $config = $container->get('config')['translator'];
+        $translator = new Translator($config['lang']);
+        $translator->addLoader('php', new PhpFileLoader());
+        $translator->addLoader('xlf', new XliffFileLoader());
 
-        return new Translator($config['lang']);
+        foreach ($config['resources'] as $resource) {
+            $translator->addResource(...$resource);
+        }
+
+        return $translator;
     },
 
     'config' => [
         'translator' => [
             'lang' => 'en',
+            'resources' => [
+                [
+                    'xlf',
+                    __DIR__ . '/../../vendor/symfony/validator/Resources/translations/validators.ru.xlf',
+                    'ru',
+                    'validators'
+                ],
+            ],
         ],
     ],
 ];
