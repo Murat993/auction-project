b7b8c7f6a3d142001e62d99bd25ce60ebbc43688
Murat <nysambaev.m>
2022-07-03 20:00:17 +0600

    Moved page to hook

--- cucumber/features/step_definitions/home.js
+++ cucumber/features/step_definitions/home.js
@@ -4,8 +4,6 @@ Given('I am a guest user', function () {});
 
 When('I open home page', async function () {
     this.page = await this.browser.newPage()
-    await this.page.setViewport({ width: 1280, height: 720 })
-    return await this.page.goto('http://gateway:8080')
 })
 
 Then('I see welcome block', function () {
