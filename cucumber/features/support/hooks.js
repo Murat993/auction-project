217a77f7285c1abc606196b7a43b2469ecd904ca
Murat <nysambaev.m>
2022-12-27 14:47:37 +0600

    Updated Cucumber

--- cucumber/features/support/hooks.js
+++ cucumber/features/support/hooks.js
@@ -8,15 +8,17 @@ Before(async function () {
             '--no-sandbox'
         ]
     })
-
     this.page = await this.browser.newPage()
     await this.page.setViewport({ width: 1280, height: 720 })
 })
 
 After(async function (testCase) {
     if (testCase.result.status === Status.FAILED) {
-        const name = testCase.sourceLocation.uri + '-' + testCase.sourceLocation.line
-        await this.page.screenshot({ path: 'var/' + name.replace(/\//g, '_') + '.png', fullPage: true })
+        const name = testCase.pickle.uri.replace(/^\/app\/features\//, '').replace(/\//g, '_') +
+            '-' +
+            testCase.pickle.name.toLowerCase().replace(/[^\w]/g, '_')
+        await this.page.screenshot({ path: 'var/' + name + '.png', fullPage: true })
     }
+    await this.page.close()
     await this.browser.close()
 })
