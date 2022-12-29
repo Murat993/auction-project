5b073a4480713ea9912761631ab7cb6ab1da1cf6
Murat <nysambaev.m>
2022-12-29 12:01:56 +0600

    Added features provider

--- frontend/src/Welcome/Welcome.jsx
+++ frontend/src/Welcome/Welcome.jsx
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react'
-import styles from './Welcome.module.css';
+import styles from './Welcome.module.css'
 import { FeaturesContext } from '../FeatureToggle'
 
 function Welcome() {
   const features = useContext(FeaturesContext)
+
   return (
     <div data-testid="welcome" className={styles.welcome}>
       <h1>Auction</h1>
@@ -13,7 +14,7 @@ function Welcome() {
         <p>We will be here soon</p>
       )}
     </div>
-  );
+  )
 }
 
-export default Welcome;
+export default Welcome
