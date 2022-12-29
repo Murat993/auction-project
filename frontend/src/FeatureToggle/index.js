0dbe95b05969cdae98917ac8dac715ae029170d5
Murat <nysambaev.m>
2022-12-29 13:39:42 +0600

    Added useFeatures hook

--- frontend/src/FeatureToggle/index.js
+++ frontend/src/FeatureToggle/index.js
@@ -1,4 +1,4 @@
-export { default as FeaturesContext } from './FeaturesContext'
 export { default as FeaturesProvider } from './FeaturesProvider'
 export { default as mergeFeatures } from './mergeFeatures'
 export { default } from './FeatureFlag'
+export { default as useFeatures } from './useFeatures'
