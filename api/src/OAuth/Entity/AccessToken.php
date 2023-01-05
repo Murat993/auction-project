e6a99ce65c121d474910411eae3727b61ec95e90
Murat <nysambaev.m>
2023-01-05 14:02:13 +0600

    Added OAuth repositories

--- api/src/OAuth/Entity/AccessToken.php
+++ api/src/OAuth/Entity/AccessToken.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\OAuth\Entity;
 
 use League\OAuth2\Server\Entities\AccessTokenEntityInterface;
+use League\OAuth2\Server\Entities\ClientEntityInterface;
+use League\OAuth2\Server\Entities\ScopeEntityInterface;
 use League\OAuth2\Server\Entities\Traits\AccessTokenTrait;
 use League\OAuth2\Server\Entities\Traits\EntityTrait;
 use League\OAuth2\Server\Entities\Traits\TokenEntityTrait;
@@ -14,4 +16,16 @@ final class AccessToken implements AccessTokenEntityInterface
     use AccessTokenTrait;
     use EntityTrait;
     use TokenEntityTrait;
+
+    /**
+     * @param ScopeEntityInterface[] $scopes
+     */
+    public function __construct(ClientEntityInterface $client, array $scopes)
+    {
+        $this->setClient($client);
+
+        foreach ($scopes as $scope) {
+            $this->addScope($scope);
+        }
+    }
 }
