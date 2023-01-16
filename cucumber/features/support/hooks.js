eec2440528bf3cad6380aa5c2e34b062d788ba12
Murat <nysambaev.m>
2023-01-16 10:35:52 +0600

    Improved E2E tests hooks

--- cucumber/features/support/hooks.js
+++ cucumber/features/support/hooks.js
@@ -1,24 +1,30 @@
 const puppeteer = require('puppeteer')
-const { Before, After, Status } = ('@cucumber/cucumber')
+const { Before, After, Status, setDefaultTimeout } = require('@cucumber/cucumber')
 
-Before(async function () {
-    this.browser = await puppeteer.launch({
-        args: [
-            '--disable-dev-shm-usage',
-            '--no-sandbox'
-        ]
-    })
-    this.page = await this.browser.newPage()
-    await this.page.setViewport({ width: 1280, height: 720 })
+setDefaultTimeout(10 * 1000)
+
+Before({ timeout: 30000 }, async function () {
+  this.browser = await puppeteer.launch({
+    args: [
+      '--disable-dev-shm-usage',
+      '--no-sandbox'
+    ]
+  })
+  this.page = await this.browser.newPage()
+  await this.page.setViewport({ width: 1280, height: 720 })
 })
 
 After(async function (testCase) {
+  if (this.page) {
     if (testCase.result.status === Status.FAILED) {
-        const name = testCase.pickle.uri.replace(/^\/app\/features\//, '').replace(/\//g, '_') +
-            '-' +
-            testCase.pickle.name.toLowerCase().replace(/[^\w]/g, '_')
-        await this.page.screenshot({ path: 'var/' + name + '.png', fullPage: true })
+      const name = testCase.pickle.uri.replace(/^\/app\/features\//, '').replace(/\//g, '_') +
+        '-' +
+        testCase.pickle.name.toLowerCase().replace(/[^\w]/g, '_')
+      await this.page.screenshot({ path: 'var/' + name + '.png', fullPage: true })
     }
     await this.page.close()
+  }
+  if (this.browser) {
     await this.browser.close()
+  }
 })
