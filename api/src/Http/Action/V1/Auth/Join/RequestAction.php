fdcafefca0d9d24e9b01ea48e69c9cbfae60ed2e
Murat <nysambaev.m>
2023-01-22 20:45:30 +0600

    Extracted denormalizer errors middleware

--- api/src/Http/Action/V1/Auth/Join/RequestAction.php
+++ api/src/Http/Action/V1/Auth/Join/RequestAction.php
@@ -7,16 +7,11 @@ namespace App\Http\Action\V1\Auth\Join;
 use App\Auth\Command\JoinByEmail\Request\Command;
 use App\Auth\Command\JoinByEmail\Request\Handler;
 use App\Http\Response\EmptyResponse;
-use App\Validator\ValidationException;
 use App\Validator\Validator;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Symfony\Component\Serializer\Exception\NotNormalizableValueException;
-use Symfony\Component\Serializer\Exception\PartialDenormalizationException;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
-use Symfony\Component\Validator\ConstraintViolation;
-use Symfony\Component\Validator\ConstraintViolationList;
 
 final class RequestAction implements RequestHandlerInterface
 {
@@ -29,28 +24,9 @@ final class RequestAction implements RequestHandlerInterface
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        try {
-            $command = $this->denormalizer->denormalize($request->getParsedBody(), Command::class, null, [
-                DenormalizerInterface::COLLECT_DENORMALIZATION_ERRORS => true,
-            ]);
-        } catch (PartialDenormalizationException $exception) {
-            $violations = new ConstraintViolationList();
-            /** @var NotNormalizableValueException $error */
-            foreach ($exception->getErrors() as $error) {
-                $message = sprintf(
-                    'The type must be one of "%s" ("%s" given).',
-                    implode(', ', (array)$error->getExpectedTypes()),
-                    (string)$error->getCurrentType()
-                );
-                $parameters = [];
-                if ($error->canUseMessageForUser()) {
-                    $parameters['hint'] = $error->getMessage();
-                }
-                $violations->add(new ConstraintViolation($message, '', $parameters, null, $error->getPath(), null));
-            }
-
-            throw new ValidationException($violations);
-        }
+        $command = $this->denormalizer->denormalize($request->getParsedBody(), Command::class, null, [
+            DenormalizerInterface::COLLECT_DENORMALIZATION_ERRORS => true,
+        ]);
 
         $this->validator->validate($command);
 
